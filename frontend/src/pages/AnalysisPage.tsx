34c504f8f23fe98492137d6f8c0dcae154a43d15
Mythri Mutyapu <mutyapu.mythri@gmail.com>
2026-06-27 21:31:13 +0530

    analysis

--- frontend/src/pages/AnalysisPage.tsx
+++ frontend/src/pages/AnalysisPage.tsx
@@ -1,18 +1,18 @@
 import { useEffect, useState } from "react";
-import { useSearchParams } from "react-router-dom";
 import { motion } from "framer-motion";
+import { useSearchParams } from "react-router-dom";
 
 import Loader from "../components/common/Loader";
 import EmptyState from "../components/common/EmptyState";
-
-import { analyzeIncident } from "../services/incidentService";
-import { AnalysisResponse } from "../types/incident";
 
 import AnalysisHero from "../components/analysis/AnalysisHero";
 import RiskScoreCard from "../components/analysis/RiskScoreCard";
 import ContextCard from "../components/analysis/ContextCard";
 import RecommendationCard from "../components/analysis/RecommendationCard";
 import ApprovalCard from "../components/analysis/ApprovalCard";
+
+import { analyzeIncident } from "../services/incidentService";
+import { AnalysisResponse } from "../types/incident";
 
 export default function AnalysisPage() {
     const [searchParams] = useSearchParams();
@@ -59,10 +59,12 @@ export default function AnalysisPage() {
     if (!incidentId) {
 
         return (
+
             <EmptyState
                 title="No Incident Selected"
-                description="Select an incident to analyze."
+                description="Please select an incident."
             />
+
         );
 
     }
@@ -76,10 +78,12 @@ export default function AnalysisPage() {
     if (!analysis || error) {
 
         return (
+
             <EmptyState
                 title="Analysis Failed"
-                description={error}
+                description={error || "No analysis available"}
             />
+
         );
 
     }
@@ -89,14 +93,23 @@ export default function AnalysisPage() {
         <motion.div
             initial={{ opacity:0 }}
             animate={{ opacity:1 }}
+            transition={{ duration:0.35 }}
             className="space-y-6"
         >
 
-            <AnalysisHero incident={analysis.incident} />
+            {/* Hero */}
+
+            <AnalysisHero
+                incident={analysis.incident}
+            />
+
+            {/* Main Grid */}
 
             <div className="grid gap-6 xl:grid-cols-3">
 
-                <div className="xl:col-span-2 space-y-6">
+                {/* Left */}
+
+                <div className="space-y-6 xl:col-span-2">
 
                     <RiskScoreCard
                         analysis={analysis.analysis}
@@ -107,6 +120,8 @@ export default function AnalysisPage() {
                     />
 
                 </div>
+
+                {/* Right */}
 
                 <div className="space-y-6">
 
